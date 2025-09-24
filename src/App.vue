219d825e7c3fae1f297b773027a95c69dfb948ec
洪国泰 <17805981973@163.com>
2025-09-24 22:22:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {getCategory} from '@/apis/test'
+getCategory().then(res=>{
+  console.log(res)
+})
+</script>
+
+<template>
+  <el-button type="primary">click</el-button>
+</template>
+
+<style scoped></style>
